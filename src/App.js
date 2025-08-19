e391619889c5ff7f7a99f6b19328b0361c13f406
rutuja.date <daterutuja5646@gmail.com>
2025-08-19 16:48:47 +0530

    multiplication by 2 in array element

--- src/App.js
+++ src/App.js
@@ -2,39 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let num1 = 10, num2 = 10;
-  let sum = 0;
-  function Addition() {
-      sum = num1 + num2;
-      console.log(sum);
-  }
+  let arr=[1,2,3,4,5,6,7,8,9,10];
   
-     function Subtraction() {
-      sum = num1 - num2;
-      console.log(sum);
-  }
-  function Multiplication() {
-      sum = num1 * num2;
-      console.log(sum);
-  }
-  function Division() {
-      sum = num1 / num2;
-      console.log(sum);
-  }
-  
-
   return (
-<div>
-  <div>{num1}</div>
-  <div>{num2}</div>
-  <button onClick={Addition}>Addition</button>
-<div><button onClick={Subtraction}>Subtraction</button>
-<div><button onClick={Multiplication}>Multiplication</button>
-<div><button onClick={Division}>Division</button></div>
-</div>
-</div>
 
-</div>
+  arr.map((SingleElement)=>{
+  return <div>{SingleElement*2}</div>
+  })
+
 
   );
 }
