7c3f6d200397b249723163e8ab74d1f689ada0e0
rutuja.date <daterutuja5646@gmail.com>
2025-08-23 11:51:15 +0530

    display image in click submit button

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [age, setAge] = useState();
-  function handleAgeChange(event) {
-    setAge(event.target.value)
+  const [name, setName] = useState();
+  const [showBlock, setShowBlock] = useState(false);
+  function onSubmit() {
+    setShowBlock(true)
+  }
+  function handleNameChange(event) {
+    setName(event.target.value)
   }
   return (
-    <div>Enter Age:
+    <div>
       <input
-        name='age' value={age}
-        onChange={handleAgeChange}
-
-      />
-      <div>{age}</div>
+        name='text' value={name}
+        onChange={handleNameChange} />
+      <div><button onClick={onSubmit}>Submit</button></div>
+      {showBlock && <div>
+        <div>{name}</div>
+        <img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-souvenirpixels-414612.jpg&fm=jpg" width="100" />
+      </div>}
 
     </div>
 
